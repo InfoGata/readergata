ca1a18c20b96c543dc982e613c552f43be59ee04
Elijah Green <elijahgreen@gmail.com>
2020-08-18 20:31:59 -0700

    Use epubjs instead of react-reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
 import React from "react";
-import { ReactReader } from "react-reader";
+import { AppBar, Toolbar, CssBaseline, Slide } from "@material-ui/core";
+import Epub, { Rendition, Book } from "epubjs";
 
 const DEMO_URL =
   "https://gerhardsletten.github.io/react-reader/files/alice.epub";
-const DEMO_NAME = "Alice in wonderland";
 
 const App: React.FC = () => {
-  const [location, setLocation] = React.useState<string | null>(null);
-  // const [menuOpen, setMenuOpen] = React.useState(false);
+  const [menuOpen, setMenuOpen] = React.useState(true);
+  const [url, setUrl] = React.useState<string | ArrayBuffer>(DEMO_URL);
+  const [rendition, setRendition] = React.useState<Rendition | null>(null);
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  const book = React.useRef<Book>();
 
-  // React.useEffect(() => {
-  //   window.addEventListener("mousemove", (event: MouseEvent) => {
-  //     if (event.pageY < 300) {
-  //       setMenuOpen(true);
-  //     }
-  //   });
-  // }, []);
-
-  const onLocationChanged = (location: string) => {
-    setLocation(location);
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
+
+  React.useEffect(() => {
+    if (book.current) {
+      book.current.destroy();
+    }
+    book.current = Epub();
+    if (url instanceof ArrayBuffer) {
+      book.current.open(url, "binary");
+    } else {
+      book.current.open(url);
+    }
+    console.log(url);
+
+    const viewer = containerRef?.current;
+    if (viewer) {
+      const rend = book.current.renderTo(viewer, {
+        width: "100%",
+        height: "100vh",
+      });
+      rend.display();
+      setRendition(rend);
+    }
+  }, [url]);
+
+  React.useEffect(() => {
+    rendition?.on("keyup", (e: KeyboardEvent) => {
+      if ((e.keyCode || e.which) === 37) {
+        rendition.prev();
+      }
+
+      // Right Key
+      if ((e.keyCode || e.which) === 39) {
+        rendition.next();
+      }
+    });
+  }, [rendition]);
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", (event: MouseEvent) => {
+      if (event.pageY < 65) {
+        setMenuOpen(true);
+      } else {
+        closeMenu();
+      }
+    });
+  }, []);
+
+  const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      if (file.type !== "application/epub+zip") {
+        return alert("Unsupported type");
+      }
+      const bookData = await file.arrayBuffer();
+      setUrl(bookData);
+    }
+  };
+
   return (
-    <div style={{ position: "relative", height: "100vh" }}>
-      <div>What</div>
-      <ReactReader
-        url={DEMO_URL}
-        title={DEMO_NAME}
-        location={location}
-        locationChanged={onLocationChanged}
-      />
-    </div>
+    <>
+      <CssBaseline />
+      <div style={{ position: "relative", height: "100vh" }}>
+        <Slide direction="down" in={menuOpen}>
+          <AppBar position="fixed" hidden={!menuOpen}>
+            <Toolbar>
+              <input type="file" onChange={onFileChange} />
+            </Toolbar>
+          </AppBar>
+        </Slide>
+        <div ref={containerRef}></div>
+      </div>
+    </>
   );
 };
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module "react-reader" {
-  const ReactReader: any;
-  const ReactReaderStyle: any;
-  export { ReactReader, ReactReaderStyle };
-}
