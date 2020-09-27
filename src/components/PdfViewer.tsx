cefa51d26def2ad544bc3eb49db0e43849304500
Elijah Green <elijahgreen@gmail.com>
2020-09-26 22:48:58 -0700

    Create table of contents for pdfs

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -1,19 +1,60 @@
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { BookContent } from "../models";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-interface IProps {
+interface Props {
   pdf: ArrayBuffer | string;
 }
 
-const PdfViewer: React.FC<IProps> = (props) => {
+interface PageLinkProps {
+  content: BookContent;
+}
+
+const PdfViewer: React.FC<Props> = (props) => {
   const [numPages, setNumPages] = React.useState(1);
   const [pageNumber, setPageNumber] = React.useState(1);
+  const [bookContents, setBookContents] = React.useState<BookContent[]>([]);
+  const [pdfDocument, setPdfDocument] = React.useState<
+    pdfjs.PDFDocumentProxy
+  >();
   const { pdf } = props;
 
-  const onDocumentLoadSuccess = (event: pdfjs.PDFDocumentProxy) => {
-    setNumPages(event.numPages);
+  React.useEffect(() => {
+    const testPage = async (outline: pdfjs.PDFTreeNode) => {
+      const [ref] = outline.dest;
+      const test: number = await (pdfDocument as any).getPageIndex(ref);
+      console.log(test);
+      return (test + 1).toString();
+    };
+
+    const outlineToContent = async (
+      outline: pdfjs.PDFTreeNode[]
+    ): Promise<BookContent[]> => {
+      if (outline.length === 0) return [];
+
+      const test = outline.map(async (o) => ({
+        title: o.title,
+        page: await testPage(o),
+        items: await outlineToContent(o.items),
+      }));
+
+      return Promise.all(test);
+    };
+
+    const onDocumentChange = async () => {
+      if (pdfDocument) {
+        const outline = await pdfDocument.getOutline();
+        setBookContents(await outlineToContent(outline));
+      }
+    };
+    onDocumentChange();
+  }, [pdfDocument]);
+
+  const onDocumentLoadSuccess = async (pdfDocument: pdfjs.PDFDocumentProxy) => {
+    setPdfDocument(pdfDocument);
+    setNumPages(pdfDocument.numPages);
     setPageNumber(1);
   };
 
@@ -29,8 +70,40 @@ const PdfViewer: React.FC<IProps> = (props) => {
     changePage(1);
   }
 
+  function onItemClick({ pageNumber }: { pageNumber: string }) {
+    if (pageNumber) {
+      setPageNumber(parseInt(pageNumber));
+    }
+  }
+
+  const PageLink: React.FC<PageLinkProps> = (props) => {
+    const onClick = () => {
+      setPageNumber(parseInt(props.content.page));
+    };
+
+    return <button onClick={onClick}>{props.content.title}</button>;
+  };
+
+  const bookContentsToList = (contents: BookContent[]) => {
+    if (contents.length === 0) {
+      return null;
+    }
+
+    return (
+      <ul>
+        {contents.map((c) => (
+          <li>
+            <PageLink content={c} />
+            {bookContentsToList(c.items)}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+      {bookContentsToList(bookContents)}
       <Page pageNumber={pageNumber} />
       <div>
         <p>
---
+++ src/models.ts
@@ -0,0 +1,5 @@
+export interface BookContent {
+  title: string;
+  page: string;
+  items: BookContent[];
+}
