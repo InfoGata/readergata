6b30fb57b9cd07f5b2630932d873ffa788d8d662
Elijah Green <elijah@infogata.com>
2022-10-25 16:03:44 -0700

    Remove broken ebook feeds

--- src/reducers/catalogReducer.ts
+++ src/reducers/catalogReducer.ts
@@ -1,20 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Catalog } from "../models";
-import { v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
 export const defaultCatalogs: Catalog[] = [
   {
-    name: "Standard Ebooks",
-    url: "https://standardebooks.org/opds",
-  },
-  {
-    name: "Feedbooks",
-    url: "https://catalog.feedbooks.com/catalog/index.atom",
-  },
-  {
     name: "Project Gutenberg",
-    url: 'https://m.gutenberg.org/ebooks.opds/',
-  }
+    url: "https://m.gutenberg.org/ebooks.opds/",
+  },
 ];
 
 interface CatalogState {
@@ -39,13 +31,10 @@ const catalogSlice = createSlice({
     },
     deleteCatalog(state, action: PayloadAction<Catalog>) {
       state.catalogs = state.catalogs.filter((c) => c.id !== action.payload.id);
-    }
+    },
   },
 });
 
-export const {
-  addCatalog,
-  deleteCatalog,
-} = catalogSlice.actions;
+export const { addCatalog, deleteCatalog } = catalogSlice.actions;
 
 export default catalogSlice.reducer;
