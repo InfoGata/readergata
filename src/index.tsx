191cabfbca251faf993563ed0e3453ed98725d90
Elijah Green <elijah@infogata.com>
2022-11-24 19:04:13 -0800

    Change font package

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "fontsource-roboto";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
