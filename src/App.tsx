e04bd16bec443dd60247311ef4688365e0460791
Elijah Green <elijah@infogata.com>
2022-12-10 16:19:55 -0800

    Move AppBar in App.tsx to TopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import React, { useEffect } from "react";
-import {
-  CssBaseline,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Grid,
-  Box,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { CssBaseline, Box } from "@mui/material";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavigationMenu from "./components/NavigationMenu";
-import {
-  setIsFullscreen,
-  setNavigationOpen,
-  setTocOpen,
-} from "./store/reducers/uiReducer";
+import { setIsFullscreen } from "./store/reducers/uiReducer";
 import screenfull, { Screenfull } from "screenfull";
-import { Toc } from "@mui/icons-material";
 import TocMenu from "./components/TocMenu";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { PluginsProvider } from "./PluginsContext";
 import Routing from "./components/Routing";
 import { QueryClient, QueryClientProvider } from "react-query";
+import TopBar from "./TopBar";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,11 +22,6 @@ const queryClient = new QueryClient({
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
-  const navigationOpen = useAppSelector((state) => state.ui.navigationOpen);
-  const tocOpen = useAppSelector((state) => state.ui.tocOpen);
-  const title = useAppSelector((state) => state.ebook.title);
-  const onNavigationToggle = () => dispatch(setNavigationOpen(!navigationOpen));
-  const onTocToggle = () => dispatch(setTocOpen(!tocOpen));
   const isFullScreen = useAppSelector((state) => state.ui.isFullscreen);
 
   useEffect(() => {
@@ -49,7 +31,7 @@ const App: React.FC = () => {
         dispatch(setIsFullscreen(sfull.isFullscreen));
       });
     }
-  });
+  }, [dispatch]);
 
   useEffect(() => {
     if (screenfull.isEnabled) {
@@ -67,35 +49,7 @@ const App: React.FC = () => {
         <PluginsProvider>
           <Box sx={{ display: "flex" }}>
             <CssBaseline />
-            <AppBar color="transparent" position="fixed">
-              <Toolbar variant="dense">
-                <IconButton
-                  color="inherit"
-                  aria-label="menu"
-                  edge="start"
-                  onClick={onNavigationToggle}
-                  sx={{ mr: 2 }}
-                  size="small"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Grid container justifyContent="center">
-                  <Typography variant="subtitle1" noWrap>
-                    {title}
-                  </Typography>
-                </Grid>
-                <IconButton
-                  color="inherit"
-                  aria-label="menu"
-                  edge="start"
-                  onClick={onTocToggle}
-                  sx={{ mr: 2 }}
-                  size="small"
-                >
-                  <Toc />
-                </IconButton>
-              </Toolbar>
-            </AppBar>
+            <TopBar />
             <NavigationMenu />
             <Routing />
             <TocMenu />
---
+++ src/TopBar.tsx
@@ -0,0 +1,49 @@
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { setNavigationOpen, setTocOpen } from "./store/reducers/uiReducer";
+import { Toc } from "@mui/icons-material";
+
+const TopBar: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const navigationOpen = useAppSelector((state) => state.ui.navigationOpen);
+  const tocOpen = useAppSelector((state) => state.ui.tocOpen);
+  const title = useAppSelector((state) => state.ebook.title);
+  const onNavigationToggle = () => dispatch(setNavigationOpen(!navigationOpen));
+  const onTocToggle = () => dispatch(setTocOpen(!tocOpen));
+
+  return (
+    <AppBar color="transparent" position="fixed">
+      <Toolbar variant="dense">
+        <IconButton
+          color="inherit"
+          aria-label="menu"
+          edge="start"
+          onClick={onNavigationToggle}
+          sx={{ mr: 2 }}
+          size="small"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Grid container justifyContent="center">
+          <Typography variant="subtitle1" noWrap>
+            {title}
+          </Typography>
+        </Grid>
+        <IconButton
+          color="inherit"
+          aria-label="menu"
+          edge="start"
+          onClick={onTocToggle}
+          sx={{ mr: 2 }}
+          size="small"
+        >
+          <Toc />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default TopBar;
