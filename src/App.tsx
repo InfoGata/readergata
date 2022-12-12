5a5e5c02c28a5ca6ce78cdc4783b7e4f8e534251
Elijah Green <elijah@infogata.com>
2022-12-12 12:31:00 -0800

    Refactor waiting service worker code into a hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavigationMenu from "./components/NavigationMenu";
 import TocMenu from "./components/TocMenu";
-import { useAppSelector } from "./store/hooks";
 import { PluginsProvider } from "./PluginsContext";
 import Routing from "./components/Routing";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -12,6 +11,7 @@ import TopBar from "./TopBar";
 import { SnackbarKey, SnackbarProvider } from "notistack";
 import { useTranslation } from "react-i18next";
 import useFullScreen from "./hooks/useFullScreen";
+import useUpdateServiceWorker from "./hooks/useUpdateServiceWorker";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,42 +22,15 @@ const queryClient = new QueryClient({
 });
 
 const App: React.FC = () => {
-  useFullScreen();
   const notistackRef = React.useRef<SnackbarProvider>(null);
-  const waitingServiceWorker = useAppSelector(
-    (state) => state.ui.waitingServiceWorker
-  );
   const { t } = useTranslation();
 
   const onClickDismiss = (key: SnackbarKey) => () => {
     notistackRef?.current?.closeSnackbar(key);
   };
 
-  React.useEffect(() => {
-    const updateServiceWorker = () => {
-      if (waitingServiceWorker) {
-        waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-
-        waitingServiceWorker.addEventListener("statechange", (e) => {
-          const sw = e?.target as ServiceWorker;
-          if (sw.state === "activated") {
-            window.location.reload();
-          }
-        });
-      }
-    };
-
-    if (waitingServiceWorker) {
-      notistackRef?.current?.enqueueSnackbar(t("newVersion"), {
-        action: (key) => (
-          <>
-            <Button onClick={updateServiceWorker}>{t("reload")}</Button>
-            <Button onClick={onClickDismiss(key)}>{t("dismiss")}</Button>
-          </>
-        ),
-      });
-    }
-  }, [waitingServiceWorker, t]);
+  useFullScreen();
+  useUpdateServiceWorker(notistackRef.current?.enqueueSnackbar, onClickDismiss);
 
   return (
     <SnackbarProvider
