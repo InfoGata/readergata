6c4ac0c5a62318c5e0c1deb156a7457b010098fa
Elijah Green <elijah@infogata.com>
2023-01-26 16:03:09 -0800

    Add plugin cards

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import DragFileContainer from "./DragFileContainer";
 import OpenFileButton from "./OpenFileButton";
+import PluginCards from "./PluginCards";
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
@@ -10,6 +11,7 @@ const Home: React.FC = () => {
     <DragFileContainer>
       <OpenFileButton />
       <Typography>{t("orDragFiles")}</Typography>
+      <PluginCards />
     </DragFileContainer>
   );
 };
--- src/default-plugins.ts
+++ src/default-plugins.ts
@@ -10,12 +10,12 @@ export const defaultPlugins: PluginDescription[] = [
     id: "P5oPcz76xlHWUtp5AzLzg",
     name: "Plugin for HumbleBundle",
     description: "Get books from Humble Bundle",
-    url: "",
+    url: "https://cdn.jsdelivr.net/gh/InfoGata/humblebundle-readergata@latest/manifest.json",
   },
   {
     id: "tJlCCR6lCfx7XeU_9LQum",
     name: "Opds Catalog",
     description: "Get books from opds catalogs.",
-    url: "",
+    url: "https://cdn.jsdelivr.net/gh/InfoGata/opds-readergata@latest/manifest.json",
   },
 ];
