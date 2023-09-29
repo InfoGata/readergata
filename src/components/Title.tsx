680e38b8c692e383f77cea47bb71b8120ead4dac
Elijah Green <elijah@infogata.com>
2023-09-29 13:21:01 -0700

    Add publication's filename to title if the document does not have one.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,11 +4,16 @@ import { Grid, Typography } from "@mui/material";
 
 const Title: React.FC = () => {
   const title = useAppSelector((state) => state.ui.title);
+  const currentPublication = useAppSelector(
+    (state) => state.document.currentPublication
+  );
+  console.log(title);
+  console.log(currentPublication?.fileName);
 
   return (
     <Grid container justifyContent="center">
       <Typography variant="subtitle1" noWrap>
-        {title}
+        {title || currentPublication?.fileName}
       </Typography>
     </Grid>
   );
